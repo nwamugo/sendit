7f35e5436b614bff2ba1aa7e60610173b1fa7ac1
Ugoji Duziem <tickettoduzz@gmail.com>
2018-11-10 11:25:24 +0100

    still Travis

---
+++ sendit-api/dist/config/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  port: process.env.PORT || 3000
+};
+exports["default"] = _default;
---
+++ sendit-api/dist/data/parcels-data.js
@@ -0,0 +1,53 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = [{
+  userId: 445,
+  parcelId: 1,
+  pickupLocation: 'Abuja',
+  destination: 'Akwa Ibom',
+  price: 56000,
+  status: 'Queue',
+  presentLocation: 'Abuja',
+  cancel: false
+}, {
+  userId: 446,
+  parcelId: 2,
+  pickupLocation: 'Ikare',
+  destination: 'Lagos',
+  price: 12000,
+  status: 'Transit',
+  presentLocation: 'Owo',
+  cancel: false
+}, {
+  userId: 447,
+  parcelId: 3,
+  pickupLocation: 'Sokoto',
+  destination: 'Warri',
+  price: 40000,
+  status: 'Queue',
+  presentLocation: 'Sokoto',
+  cancel: false
+}, {
+  userId: 448,
+  parcelId: 4,
+  pickupLocation: 'Calabar',
+  destination: 'Obudu',
+  price: 2000,
+  status: 'Delivered',
+  presentLocation: 'Obudu',
+  cancel: false
+}, {
+  userId: 449,
+  parcelId: 5,
+  pickupLocation: 'Enugu',
+  destination: 'Alaba',
+  price: 15000,
+  status: 'Transit',
+  presentLocation: 'Sagamu',
+  cancel: false
+}];
+exports["default"] = _default;
