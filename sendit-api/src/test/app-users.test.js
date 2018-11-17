cb3abc102f8b2c2bc3a7bc49e3ee60328972f3d4
Ugoji Duziem <tickettoduzz@gmail.com>
2018-11-17 09:09:40 +0100

    by a specific user get all

---
+++ sendit-api/src/test/app-users.test.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import supertest from 'supertest';
+import app from '..';
+
+import parcels from '../data/parcels-data';
+
+
+const api = supertest(app);
+
+describe('User test', () => {
+  after(() => app.server.close());
+  describe('GET /users/:userId/parcels', () => {
+    it('should fetch all parcel delivery orders by a specific user', (done) => {
+      api.get(`/api/v1/users/${parcels[0].userId}/parcels`)
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body[0].userId).to.equal(parcels[0].userId);
+          done();
+        });
+    });
+  });
+});
